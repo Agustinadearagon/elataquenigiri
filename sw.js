ca5dbe9f2cd7beb8e2bf9c2ebfe169868533260e
Agustinadearagon <72619859+Agustinadearagon@users.noreply.github.com>
2026-08-13 01:54:06 +0200

    Change CACHE_NAME from 'nigiri-v4' to 'nigiri-v3'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nigiri-v4";
+const CACHE_NAME = "nigiri-v3";
 const FILES = ["./", "./index.html", "./script.js", "./style.css", "./cara.jpg", "./sw.js"];
 
 self.addEventListener("install", e => {
